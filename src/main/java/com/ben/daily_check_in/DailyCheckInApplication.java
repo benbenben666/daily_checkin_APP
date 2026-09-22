a92e2f3a38f1d6f449de191bf331fd03311140f0
ben <3460699480@qq.com>
2026-09-22 23:35:52 +0800

    初次尝试

---
+++ src/main/java/com/ben/daily_check_in/DailyCheckInApplication.java
@@ -0,0 +1,13 @@
+package com.ben.daily_check_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCheckInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCheckInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ben/daily_check_in/DailyCheckInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ben.daily_check_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCheckInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
